a3688d652c8e7022eed9070a6fc876d0fb0bad8a
fly <Benylaranjinha@gmail.com>
2025-11-23 18:56:15 +0000

    Nodes And Tree Printing

---
+++ Assets/Scripts/BehaviourTree/RobberBehaviour.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RobberBehaviour : MonoBehaviour
+{
+    BehaviourTree tree;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tree = new BehaviourTree();
+        Node steal = new Node("Steal Something");
+        Node goToDiamond = new Node("Go To Diamond");
+        Node goToVan = new Node("Go To Van");
+
+        steal.AddChild(goToDiamond);
+        steal.AddChild(goToVan);
+        tree.AddChild(steal);
+
+        Node eat = new Node("Eat Something");
+        Node pizza = new Node("Go To Pizza Shop");
+        Node buy = new Node("Buy Pizza");
+
+        eat.AddChild(pizza);
+        eat.AddChild(buy);
+        tree.AddChild(eat);
+
+        tree.PrintTree();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
